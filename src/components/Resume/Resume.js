ab3ed018f1b698fe53944d366924a3024c0c4320
Mashaan <mashaan.awad@outlook.com>
2024-11-03 09:04:48 +0300

    update thumbnails and logos

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -38,24 +38,6 @@ function Resume() {
   return (
     <div>
       <Container fluid className="resume-section">
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-        
-       {/* <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row> */}
-
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center" onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
