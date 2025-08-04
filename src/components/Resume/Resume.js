fafb39d83a4d30e724fbdf4b2cbeefe484881100
Mashaan <mashaan.awad@outlook.com>
2025-08-04 03:26:44 +0300

    update CV

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -42,6 +42,7 @@ function Resume() {
           <Document file={pdf} className="d-flex justify-content-center" onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+          {/*
           <div>
             <p>
               Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
@@ -60,7 +61,8 @@ function Resume() {
             >
               Next
             </button>
-          </div>          
+          </div>
+          */}          
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
